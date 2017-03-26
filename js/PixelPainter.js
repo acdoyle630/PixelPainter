e17093b90c6c08a8160cdabcf33627688de2ad5d
Adam <acdoyle630@gmail.com>
2017-03-25 19:42:31 -1000

    background

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,16 +1,19 @@
 var grid = document.getElementById('pixelPainter');
-
+document.body.style.backgroundImage =  'http://hd-wall-papers.com/images/wallpapers/art-background/art-background-2.jpg';
 
 function pixelPainter() {
-  var size = 10;
+  var size = 30;
   var rows = 0;
   var currentPixel = null;
   currentColor = null;
   var drag = false;
-  brushSize = 1;
+  var stampOn = false;
 
   // creates buttons for div row
   function createTableRow( ){
+    var canvas = document.createElement('div');
+    canvas.id = "canvas";
+    grid.appendChild(canvas);
     var rowNumber = 0;
     for(var i=0; i<size; i++){
       var newButton = document.createElement( 'button' );
@@ -21,17 +24,17 @@ function pixelPainter() {
       newButton.addEventListener('mousedown', function(){
         drag = true;
         this.style.backgroundColor = currentColor;
-        console.log(drag);
+        console.log('drag' + drag);
       });
       newButton.addEventListener('mouseover', function(){
-        if(drag === true){
+        if(drag ===true){
         this.style.backgroundColor = currentColor;
         }
       });
       newButton.addEventListener('mouseup', function(){
         drag = false;
       });
-      grid.appendChild(newButton);
+      canvas.appendChild(newButton);
       }
 
   }
@@ -39,13 +42,20 @@ function pixelPainter() {
   function createDiv( ){
     for(var i=0; i<size; i++){
     rows += size;
-    grid.appendChild(document.createElement( 'div' ) );
+    var palat = grid.appendChild(document.createElement( 'div' ) );
+    palat.id = 'canvas';
     createTableRow(size);
     }
   }
     // creates color div/button
   function colorDiv() {
     var newDiv = grid.appendChild(document.createElement ( 'div' ) );
+    newDiv.id = 'colorGrid';
+    var newDiv2 = grid.appendChild(document.createElement ( 'div' ) );
+    newDiv.id = 'colorGrid2';
+    var newDiv3 = grid.appendChild(document.createElement ( 'div' ) );
+    newDiv.id = 'colorGrid3';
+
 
     var selected = document.createElement( 'button' );
     selected.id = 'selected';
@@ -60,47 +70,62 @@ function pixelPainter() {
     newButton.style.backgroundColor = 'blue';
     newButton.innerHTML = 'BLUE';
     newDiv.appendChild(newButton);
+
+    var newButton8 = document.createElement( 'button' );
+    newButton8.id = 'black';
+    newButton8.className = 'color';
+    newButton8.style.backgroundColor = 'black';
+    newButton8.innerHTML = 'BLACK';
+    newDiv.appendChild(newButton8);
+
     var newButton2 = document.createElement( 'button' );
     newButton2.id = 'red';
     newButton2.className = 'color';
     newButton2.style.backgroundColor ='red';
     newButton2.innerHTML = 'RED';
-    newDiv.appendChild(newButton2);
+    newDiv2.appendChild(newButton2);
     var newButton3 = document.createElement( 'button' );
     newButton3.id = 'green';
     newButton3.className = 'color';
     newButton3.style.backgroundColor ='green';
     newButton3.innerHTML = 'GREEN';
-    newDiv.appendChild(newButton3);
+    newDiv2.appendChild(newButton3);
     var newButton4 = document.createElement( 'button' );
-    newButton4.id = 'orange';
+    newButton4.id = 'transparent';
     newButton4.className = 'color';
-    newButton4.style.backgroundColor ='orange';
-    newButton4.innerHTML = 'ORANGE';
-    newDiv.appendChild(newButton4);
+    newButton4.style.backgroundColor ='ORange';
+    newButton4.innerHTML = 'MYSTERY';
+    newDiv2.appendChild(newButton4);
     var newButton5 = document.createElement( 'button' );
     newButton5.id = 'white';
     newButton5.className = 'color';
     newButton5.innerHTML = 'CLEAR';
     newButton5.style.backgroundColor ='white';
-    newDiv.appendChild(newButton5);
+    newDiv3.appendChild(newButton5);
     var newButton6 = document.createElement( 'button' );
     newButton6.id = 'clear';
     newButton6.className = 'color';
     newButton6.innerHTML = 'CLEAR ALL';
     newButton6.style.backgroundColor ='white';
-    newDiv.appendChild(newButton6);
+    newDiv3.appendChild(newButton6);
 
     var newButton7 = document.createElement( 'button' );
     newButton7.id = 'stamp';
-    newButton7.className = 'stamp';
+    newButton7.className = 'color';
     newButton7.innerHTML = 'STAMP';
     newButton7.style.backgroundColor ='white';
-    newDiv.appendChild(newButton7);
+    newDiv3.appendChild(newButton7);
     newButton7.addEventListener('click', function(){
-      stamp();
+      console.log('stamp' +stampOn);
+      stampOn = !stampOn;
+      console.log('stamo' + stampOn);
+      if(stampOn === true){
+        stamp();
+      } else {
+        unStamp();
+      }
     });
-    }
+  }
 
 
   // saves pixel Color to currentColor on click
@@ -126,7 +151,6 @@ function pixelPainter() {
   }
 
   function stamp (  ){
-    var stamp = [];
     var brush = document.addEventListener('click', function(e) {
       var next =((e.target.id));
       test = Number(next) + size;
@@ -149,13 +173,28 @@ function pixelPainter() {
     });
   }
 
+
+function unStamp(){
+  var point = document.addEventListener('click', function(e) {
+    var current = (e.target.id);
+    var point = document.getElementById(current);
+    (function(){
+    point.style.backgroundColor = currentColor;
+  })();
+  });
+}
+
+
   return {
     createTableRow,
     colorDiv,
     createDiv,
     selectColor,
     stamp,
+    unStamp
    };
+
+
 }
 
 var render = pixelPainter();
